47b9c4cdeb3ed122c54e2b4fa0e2e3ce02167c3f
Henry Zhang <62298214+HenryZhang-ZHY@users.noreply.github.com>
2025-06-03 21:41:56 +0800

    ui: reading list

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,22 @@
-import { glob } from 'astro/loaders';
+import { glob, file } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 	}),
 });
 
-export const collections = { blog };
+const book = defineCollection({
+	loader: file('./src/content/book.yaml'),
+	schema: z.object({
+		title: z.string(),
+		url: z.string().url(),
+	}),
+});
+
+export const collections = { blog, book };
