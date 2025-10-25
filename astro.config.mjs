df51901591af0d9b3987ad91d02977c72869ea91
Henry Zhang <62298214+HenryZhang-ZHY@users.noreply.github.com>
2025-10-25 21:20:43 +0800

    ui: remove unwanted code block margin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,27 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import expressiveCode from 'astro-expressive-code';
+import { addClassName } from 'astro-expressive-code/hast';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shuzhi.zone',
-  integrations: [expressiveCode(), mdx(), sitemap()],
+  integrations: [
+    expressiveCode({
+      plugins: [
+        {
+          name: 'not-content-plugin',
+          hooks: {
+            postprocessRenderedBlock: ({ renderData }) => {
+              addClassName(renderData.blockAst, 'not-content');
+            }
+          }
+        }
+      ]
+    }),
+    mdx(),
+    sitemap()
+  ],
   markdown: {
     shikiConfig: {
       themes: {
